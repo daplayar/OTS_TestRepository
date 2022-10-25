3b34d5768798c70e7313ea3ffdfdb6276f4cca02
daplayar <daplayar@web.de>
2022-10-25 20:48:35 +0200

    first_attempt

---
+++ simple_translator.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+int main() {
+	
+	std::string ger1;
+	std::string fra1 = "Sacados";
+
+	std::cin >> ger1;
+	std::cout << "I do not care what you want to translate to french,\n but your translation will always read:\n " << fra1;
+
+		return 0;
+}
